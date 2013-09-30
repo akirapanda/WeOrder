304671f9845661c4ca0caeee0afad102168a9f34
Akira Panda <p.chenliang@gmail.com>
2013-09-30 20:10:47 +0800

    add weixin

---
+++ app/controllers/weixin_controller.rb
@@ -0,0 +1,21 @@
+class WeixinController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	#before_filter :check_weixin_legality
+	def show
+		render :text=> params[:echostr]
+	end
+	
+	def create
+		logger.debug "text:#{params[:xml]}"
+		if params[:xml][:MsgType]=="text"
+		  @content="Hello Weixin"
+			render "echo",:format=>:xml
+		end
+	end
+	
+	def check_weixin_legality
+	        #logger.debug params[:timestamp]
+		array =["testtoken",params[:timestamp],params[:nonce]].sort
+		render :text => "Forbidden",:status => 403 if params[:signature]!=Digest::SHA1.hexdigest(array.join)
+	end
+end
