cac5d76b9592cbaa650a8b1b5f102ef0de503df7
Akira Panda <p.chenliang@gmail.com>
2013-10-01 21:17:07 +0800

    goods part finished

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,6 @@
 require 'carrierwave/orm/activerecord'
 class Good < ActiveRecord::Base
   validates :name, :content, :price, :unit,:presence => true
-  
+  validates :price, :numericality =>{:greater_than_or_equal_to=>0.01}
   mount_uploader :cover,CoverUploader
 end
