9864864336c4c92b925d8c56306d8972473bb57c
Akira Panda <p.chenliang@gmail.com>
2013-10-06 22:46:25 +0800

    fix a bug

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,6 +1,6 @@
 class GoodsController < ApplicationController
   before_action :set_good, only: [:show, :edit, :update, :destroy,:photo,:buyer,:comments]
-  before_filter :authenticate_user!,except: [:show,:comments,:buy]
+  before_filter :authenticate_user!,except: [:show,:comments,:buyer]
   # GET /goods
   # GET /goods.json
   def index
