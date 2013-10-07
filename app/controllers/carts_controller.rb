fdbeea10dc5c495b30d7657aedcd8f0582feeade
Akira Panda <p.chenliang@gmail.com>
2013-10-07 21:53:17 +0800

    if blank cart#show ,system would hide the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,6 @@
 class CartsController < ApplicationController
   before_action :set_cart, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!,only: [:edit,:index]
 
   # GET /carts
   # GET /carts.json
