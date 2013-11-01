1b1a7f77be15ad09c6bd42b8bb5b428cd65665e7
Akira Panda <p.chenliang@gmail.com>
2013-11-01 22:28:14 +0800

    change admin list

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :goods
   def admin?
     if email=="p.chenliang@gmail.com" || email=="525483886@qq.com" || email== "xoyljlj@126.com" ||
-       email=="670390177@qq.com" || email=="630131222@qq.com "
+       email=="670390177@qq.com" || email=="630131222@qq.com"
       return true
     end
   end
