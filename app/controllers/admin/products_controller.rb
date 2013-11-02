8eef259490afd25dcf06ffe4d15c675b443ceaff
Akira Panda <p.chenliang@gmail.com>
2013-11-02 23:14:02 +0800

    add export unit

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,7 +3,7 @@ class Admin::ProductsController < Admin::BaseController
     @products = Product.paginate(:page => params[:page], :per_page => 20)
     respond_to do |format| 
       format.html {}
-      format.csv { send_data(Product.all.to_csv(:only => [:id,:name,:price]))} 
+      format.csv { send_data(Product.all.to_csv(:only => [:id,:name,:price,:unit]))} 
     end
   end
 end
