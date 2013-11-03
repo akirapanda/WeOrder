b3bd589b6757bd0d0dfeaaea9147713714100d20
Akira Panda <p.chenliang@gmail.com>
2013-11-03 18:36:54 +0800

    m

--- db/migrate/20131103102321_add_price_into_shopping_items.rb
+++ db/migrate/20131103102321_add_price_into_shopping_items.rb
@@ -1,7 +1,7 @@
 class AddPriceIntoShoppingItems < ActiveRecord::Migration
   def change
-    add_column :shopping_items,:price,:deciaml, :precision => 10, :scale => 2  ,:default=>0
+    add_column :shopping_items,:price,:decimal, :precision => 10, :scale => 2  ,:default=>0
     add_column :users,:role ,:string
-    add_column :shoppings,:actual_amount,:deciaml,:precision => 10, :scale => 2  ,:default=>0
+    add_column :shoppings,:actual_amount,:decimal,:precision => 10, :scale => 2  ,:default=>0
   end
 end
