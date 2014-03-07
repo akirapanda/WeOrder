06e6c400e5c5a4bf4d24a5306e13956ad0b5c4cc
Akira Panda <p.chenliang@gmail.com>
2014-03-07 21:32:39 +0800

    add new admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
   has_many :products
   def admin?
     if email=="p.chenliang@gmail.com" || email=="525483886@qq.com" || email== "xoyljlj@126.com" ||
-       email=="670390177@qq.com" || email=="630131222@qq.com"
+       email=="670390177@qq.com" || email=="630131222@qq.com" || email == "419717659@qq.com" || 
+       email == "839483985@qq.com" || email == "dxnycxg@hotmail.com" || email =="1012988215@qq.com"
       return true
     end
   end
