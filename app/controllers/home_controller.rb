dd33a9a71b3afc6886ee966ee34e92ee92636063
Akira Panda <p.chenliang@gmail.com>
2014-03-21 16:57:09 +0800

    add

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,15 @@ class HomeController < ApplicationController
   layout 'shop'
   def index
     @cart=current_cart
+    @shop = Shop.find(1)
   end
   
   
   def test
     @cart=current_cart    
     @q=Product.search(params[:q])
+    @shop = Shop.find(1)
+    
   end
   
   
