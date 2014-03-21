e714c1f54c156369d11ee86a6c8a4603cb1f4e8a
Akira Panda <p.chenliang@gmail.com>
2014-03-21 16:51:57 +0800

    change the dilevety time

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -2,7 +2,7 @@ class Shopping < ActiveRecord::Base
   acts_as_paranoid
   #BUILDS=["一食堂票务亭","一公寓","二公寓","三公寓","四公寓","五公寓","三宿舍","四宿舍","五宿舍","六宿舍","七宿舍","八宿舍","九宿舍","十二宿舍","南校区","其他"]
   BUILDS=["一食堂票务亭"]
-  RECEIVE_TIMES=["12:15 - 19:50","18:00 - 19:50","21:00 - 22:00"]
+  RECEIVE_TIMES= ["12:00","18:00","20:30","次日12:15 - 19:50"]
   STATUS=["新订单","处理中","配送完毕"]
   SCHOOL_AREAS = ["南校区","北校区"]
   has_many :shopping_items,:dependent => :destroy
@@ -26,16 +26,16 @@ class Shopping < ActiveRecord::Base
   
   def self.available_time
     #0~11:00  
-    if (0..10) === Time.now.hour
+    if (0..11) && (0...30)===Time.now.min === Time.now.hour
       return RECEIVE_TIMES
     #11:00~17:15
-    elsif (11..17) === Time.now.hour || (17 === Time.now.hour && (0...15)===Time.now.min)
-      return ["18:00 - 19:50","21:00 - 22:00","次日12:15 - 19:50"]
+    elsif (11 === Time.now.hour && (30...59) == Time.now.min) || ( (12...17) === Time.now.hour && (0...30)===Time.now.min)
+      return ["18:00","20:30","次日12:00"]
     #17:15 ~ 0:00
-    elsif  Time.now.hour > 17 ||  (17 === Time.now.hour && Time.now.min >= 15 )
-      return ["次日12:15 - 19:50","次日18:00 - 19:50","次日21:00 - 22:00"]
-    else
-      return ["次日12:15 - 19:50","次日18:00 - 19:50","次日21:00 - 22:00"]
+   elsif (17 === Time.now.hour && (30...59) == Time.now.min) || ( (18...20) === Time.now.hour)
+      return ["20:30","次日12:00","次日18:00"]
+   else
+      return ["次日12:00","次日18:00","次日20:30"]
     end
   end
   
