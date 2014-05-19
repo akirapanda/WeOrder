836b8532061f431512e21e2fc84b0ad512ee45b6
Akira Panda <p.chenliang@gmail.com>
2014-05-19 11:28:44 +0800

    change jianjian mail

---
+++ app/assets/javascripts/shopey.js
@@ -0,0 +1,6 @@
+//= require mobile/jquery.min
+//= require mobile/contact
+//= require mobile/custom
+//= require mobile/effects.jquery-ui.min
+//= require mobile/jquery.colorbox.min
+//= require mobile/jquery.nivo-slider.min
